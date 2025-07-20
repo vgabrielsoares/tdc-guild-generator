c5baf4b5c1f2f1ab8146e840ecb46ebce0e1815b
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-07-19 23:55:53 -0300

    wip: add ServiceList component for displaying services

---
+++ src/components/services/ServiceList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="service-list p-6 bg-gray-800 rounded-lg border border-gray-700">
+    <div class="text-center">
+      <h3 class="text-xl font-semibold text-amber-400 mb-4">
+        ⚔️ Lista de Serviços
+      </h3>
+      <p class="text-gray-300">
+        Componente será implementado na Issue 5.5
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// ServiceList component
+// Will be implemented in Issue 5.5
+console.log('⚔️ ServiceList component loaded')
+</script>
