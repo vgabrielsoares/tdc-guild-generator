9a446531f39c46e26e7038050fd44a936b0b8874
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-07-19 23:55:43 -0300

    wip: add RenownTracker component for tracking renown

---
+++ src/components/renown/RenownTracker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="renown-tracker p-6 bg-gray-800 rounded-lg border border-gray-700">
+    <div class="text-center">
+      <h3 class="text-xl font-semibold text-amber-400 mb-4">
+        ⭐ Rastreador de Renome
+      </h3>
+      <p class="text-gray-300">
+        Componente será implementado na Issue 8.5
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// RenownTracker component
+// Will be implemented in Issue 8.5
+console.log('⭐ RenownTracker component loaded')
+</script>
