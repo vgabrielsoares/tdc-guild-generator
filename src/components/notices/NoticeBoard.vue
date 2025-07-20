0c6efb81a1c25cd8f1f709e76972e4afa12d62af
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-07-19 23:55:27 -0300

    wip: add NoticeBoard component for displaying notices

---
+++ src/components/notices/NoticeBoard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="notice-board p-6 bg-gray-800 rounded-lg border border-gray-700">
+    <div class="text-center">
+      <h3 class="text-xl font-semibold text-amber-400 mb-4">
+        📰 Mural de Avisos
+      </h3>
+      <p class="text-gray-300">
+        Componente será implementado na Issue 7.5
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// NoticeBoard component
+// Will be implemented in Issue 7.5
+console.log('📰 NoticeBoard component loaded')
+</script>
