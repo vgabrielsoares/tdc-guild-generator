42001a3816abfdb5b6532a7c8d8f3d3ba00ac541
victor.gabriel <victor.gabriel@saude.se.gov.br>
2025-07-28 14:13:43 -0300

    #13 - feat: add headquarters badge to guild info card

--- src/components/guild/GuildInfoCard.vue
+++ src/components/guild/GuildInfoCard.vue
@@ -9,12 +9,28 @@
         <h3 class="guild-info-card__title">
           {{ guild.name }}
         </h3>
-        <div 
-          v-if="showBadge"
-          class="guild-info-card__badge"
-          :class="badgeClasses"
-        >
-          {{ guild.settlementType }}
+        <div class="guild-info-card__badges">
+          <div 
+            v-if="showBadge"
+            class="guild-info-card__badge"
+            :class="badgeClasses"
+          >
+            {{ guild.settlementType }}
+          </div>
+          <div 
+            v-if="guild.structure.isHeadquarters"
+            class="guild-info-card__badge guild-info-card__badge--headquarters"
+          >
+            <svg 
+              class="inline-block w-3 h-3 mr-1" 
+              fill="currentColor" 
+              viewBox="0 0 20 20" 
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>
+            Sede Matriz
+          </div>
         </div>
       </div>
       
@@ -244,6 +260,14 @@ function formatDate(date: Date): string {
 
 .guild-info-card__badge--metropole {
   @apply bg-amber-900 text-amber-300;
+}
+
+.guild-info-card__badge--headquarters {
+  @apply bg-gradient-to-r from-yellow-600 to-amber-600 text-yellow-100 font-bold shadow-lg border border-yellow-500;
+}
+
+.guild-info-card__badges {
+  @apply flex flex-wrap gap-2;
 }
 
 .guild-info-card__actions {
