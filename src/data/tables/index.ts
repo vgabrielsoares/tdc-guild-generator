37aeccbcb7fecf9b7c7c77ac99e847963b32e371
Victor Gabriel Soares <4vgabriel@gmail.com>
2025-08-01 23:56:26 -0300

    #32 - feat: export contract-modifier-tables in central data tables index

--- src/data/tables/index.ts
+++ src/data/tables/index.ts
@@ -2,6 +2,7 @@
 export * from './guild-structure'
 export * from './contract-tables'
 export * from './contract-base-tables'
+export * from './contract-modifier-tables'
 export * from './service-tables'
 export * from './member-tables'
 export * from './notice-tables'
