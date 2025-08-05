7534a040a58c0b279a4e3e59e64c7c650822ab0e
victor.gabriel <victor.gabriel@saude.se.gov.br>
2025-08-05 15:00:22 -0300

    chore: remove dangerous contract indicator from ContractCard component

--- src/components/contracts/ContractCard.vue
+++ src/components/contracts/ContractCard.vue
@@ -126,14 +126,6 @@
         </button>
       </div>
     </div>
-
-    <!-- Indicador de contrato perigoso -->
-    <div
-      v-if="isDangerous"
-      class="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-2 py-1 rounded-full shadow-lg"
-    >
-      {{ contract.difficulty }}
-    </div>
   </div>
 </template>
 
